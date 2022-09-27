5c80a7e23e3982af2982d2bdae958124dca82595
Calle20 <97348165+Calle20@users.noreply.github.com>
2022-09-27 16:04:09 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Container
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(12, 55);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(166, 40);
+            this.lblText.TabIndex = 4;
+            this.lblText.Text = "Schwerster Container \r\nbestimmbar?";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(166, 40);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Datei öffnen";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(31, 104);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(116, 20);
+            this.lblOut.TabIndex = 5;
+            this.lblOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 139);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace Container
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt";
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileContent = File.ReadAllText(filePath);
+                fileContent.Trim();
+                HashSet<int> different_numbers=new HashSet<int>();
+                Dictionary<int, HashSet<int>> containers = new Dictionary<int, HashSet<int>>();
+                List<int> firstColumn=new List<int>();
+                List<int> secondColumn=new List<int>();
+                foreach(string currentComparison in fileContent.Split('\n'))
+                {
+                    if (currentComparison == "")
+                    {
+                        break;
+                    }
+                    int i = 0;
+                    foreach(var currentString in currentComparison.Split(' '))
+                    {
+                        int current_number=int.Parse(currentString);
+                        different_numbers.Add(current_number);
+                        if (!containers.ContainsKey(current_number))
+                        {
+                            containers.Add(current_number, new HashSet<int>());
+                        }
+                        
+                        if(i == 0) 
+                        {
+                            firstColumn.Add(current_number);
+                        }
+                        else
+                        {
+                            secondColumn.Add(current_number);
+                        }
+                        i++;
+                    }
+                }
+                var iterList = firstColumn.Zip(secondColumn,(f, s) => new { firstColumn = f, secondColumn = s });
+                foreach (var firstAndSecondColumn in iterList)
+                {
+                    containers.TryGetValue(firstAndSecondColumn.firstColumn, out var firstContainerSet);
+                    containers.TryGetValue(firstAndSecondColumn.secondColumn, out var secondContainerSet);
+
+                    firstContainerSet.Add(firstAndSecondColumn.secondColumn);
+                    foreach (var i in secondContainerSet)
+                    {
+                        firstContainerSet.Add(i);
+                    }
+
+                    foreach(var hashSet in containers.Values)
+                    {
+                        if (hashSet.Contains(firstAndSecondColumn.firstColumn))
+                        {
+                            foreach(var i in firstContainerSet)
+                            {
+                                hashSet.Add(i);
+                            }
+                        }
+                    }
+                }
+                bool isBiggestContainer = false;
+
+                foreach (var (key,value) in containers.Select((k,v)=>(k,v)))
+                {
+                    if (key.Value.Count == different_numbers.Count - 1)
+                    {
+                        isBiggestContainer = true;
+                        lblOut.Text = "Ja: Nummer " + key.Key.ToString();
+                    }
+                }
+                if (!isBiggestContainer)
+                {
+                    lblOut.Text = "Nein";
+                }
+            }
+        }
+    }
+}
